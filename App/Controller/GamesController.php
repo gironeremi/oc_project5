c7822335124654bcd69b913632a6de4089ed0535
GIRONE <gironeremi@gmail.com>
2020-12-24 07:06:30 +0100

    Ajout "d'ajout d'image".

--- App/Controller/GamesController.php
+++ App/Controller/GamesController.php
@@ -9,6 +9,42 @@ class GamesController extends Controller
         if ($_SESSION['role'] === 'admin') {
             $gameName = $this->cleanVar($_POST['gameName']);
             $style = $this->cleanVar($_POST['style']);
+            $target_dir = 'public/images/';
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            if (isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                if ($check !== false) {
+                    //echo "Le fichier est une image - " . $check["mime"] . ".";
+                    $uploadOk = 1;
+                } else {
+                    $uploadOk = 0;
+                    throw new \Exception('le fichier n\'est pas une image.');
+                }
+            }
+            if (file_exists($target_file)) {
+                $uploadOk = 0;
+                throw new \Exception('le fichier existe déjà');
+            }
+            if ($_FILES["fileToUpload"]["size"] > 500000) {
+                $uploadOk = 0;
+                throw new \Exception('Le fichier est trop gros. La limite est de 500Ko.');
+            }
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+                throw new \Exception('Seuls les fichiers JPG, PNG et JPEG sont autorisés');
+            }
+            if ($uploadOk == 0) {
+                throw  new \Exception('Désolé, le fichier n\'a pu être transmis.');
+            } else {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    $successMessage = "Le fichier " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " a été transmis.";
+                } else {
+                    throw  new \Exception('Désolé, le fichier n\'a pu être transmis.');
+                }
+            }
             if (empty($gameName) || empty($style)) {
                 throw new \Exception('Toutes les données ne sont pas saisies!');
             } else {
@@ -17,7 +53,7 @@ class GamesController extends Controller
                 if ($newGame === false) {
                     throw new \Exception('impossible d\'ajouter ce jeu!');
                 } else {
-                    $successMessage = "Nouveau jeu de rôle ajouté!";
+                    $successMessage .= "<br />Nouveau jeu de rôle ajouté!";
                     require('View/template.php');
                     die();
                 }
--- View/gameEditorView.php
+++ View/gameEditorView.php
@@ -1,6 +1,6 @@
 <?php $title = 'Éditeur de jeu'; ?>
 <?php ob_start(); ?>
-    <form class="m-3 p-3 shadow" method="post" <?php
+    <form class="m-3 p-3 shadow" method="post" enctype="multipart/form-data" <?php
     if (!empty($game)) {?>
         action="index.php?action=updateGame&game_id=<?= $game['game_id'] ?>"
     <?php
@@ -11,25 +11,32 @@
     }
     ?>
     >
-    <div class="form-group row">
+    <div class="form-group row p-2">
         <label for="gameName" class="col-4 col-form-label">Nom du jeu</label>
         <div class="col-8">
             <input id="gameName" name="gameName" placeholder="écrire ici" type="text" class="form-control" aria-describedby="gameNameHelpBlock" maxlength="255" required="required" value="<?php if(!empty($game['gameName'])) {echo $game['gameName'];} ?>">
             <span id="gameNameHelpBlock" class="form-text text-muted">N'oubliez pas les majuscules!</span>
         </div>
     </div>
-    <div class="form-group row">
+    <div class="form-group row p-2">
         <label for="style" class="col-4 col-form-label">Un court descriptif du jeu</label>
         <div class="col-8">
             <input id="style" name="style" placeholder="écrire ici aussi" type="text" class="form-control" aria-describedby="styleHelpBlock" maxlength="255" required="required" value="<?php if(!empty($game['style'])) {echo $game['style'];} ?>">
             <span id="styleHelpBlock" class="form-text text-muted">style de jeu, nature de l'univers, etc.</span>
         </div>
     </div>
-    <div class="form-group row">
+
+    <div class="form-group row p-2">
         <div class="offset-4 col-8">
-            <button name="submit" type="submit" class="btn btn-primary">Ajouter à la liste de jeux</button>
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <span id="fileToUploadHelpBlock" class="form-text text-muted">Seulement des images, 500Ko maximum!</span>
         </div>
     </div>
+        <div class="form-group row p-2">
+            <div class="offset-4 col-8">
+                <button name="submit" type="submit" class="btn btn-primary">Ajouter à la liste de jeux</button>
+            </div>
+        </div>
 </form>
 
 <?php $content = ob_get_clean();
